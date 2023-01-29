ddbc0491b726082c6e8aaea37c81db1199dc8ede
garydsong <ceouls@gmail.com>
2023-01-29 11:14:13 -0800

    added mixed active and inactive

--- technical.js
+++ technical.js
@@ -15,9 +15,39 @@ const users = [
     }
 ]
 
+const users2 = [
+    {
+        id: 1,
+        name: "user1",
+        customer_id: 2,
+        activatedOn: new Date('December 17, 2019'),
+        deactivatedOn: null
+    },
+    {
+        id: 2,
+        name: "user2",
+        customer_id: 2,
+        activatedOn: new Date('October 17, 2020'),
+        deactivatedOn: new Date('October 17, 2021')
+    },
+    {
+        id: 3,
+        name: "user3",
+        customer_id: 2,
+        activatedOn: new Date('June 23, 2009'),
+        deactivatedOn: new Date('October 17, 2018')
+    }
+
+]
+
 const activatedUser = {
     id: 1,
     ratePerMonth: 4
+}
+
+const activatedUser2 = {
+    id: 2,
+    ratePerMonth: 5
 }
 
 
@@ -26,14 +56,25 @@ function activatedDateParser(users, active) {
     let total = 0;
 
     for (let i = 0; i < users.length; i++) {
-        const customerActivationDate = users[i].activatedOn;
-        const timeDifference = currentDate - customerActivationDate;
-        const daysDifference = Math.ceil(timeDifference / (1000 * 60 * 60 * 24));
-        const monthsDifference = Math.ceil(timeDifference / (1000 * 60 * 60 * 24 * 30));
-        total += active.ratePerMonth * (daysDifference / (30 * monthsDifference));
+        if (!users[i].deactivatedOn) {
+            const customerActivationDate = users[i].activatedOn;
+            const timeDifference = currentDate - customerActivationDate;
+            const daysDifference = Math.ceil(timeDifference / (1000 * 60 * 60 * 24));
+            const monthsDifference = Math.ceil(timeDifference / (1000 * 60 * 60 * 24 * 30));
+            total += active.ratePerMonth * (daysDifference / (30 * monthsDifference));
+        } else {
+            const customerActivationDate = users[i].activatedOn;
+            const customerDeactivationDate = users[i].deactivatedOn;
+            const timeDifference = customerDeactivationDate - customerActivationDate;
+            const daysDifference = Math.ceil(timeDifference / (1000 * 60 * 60 * 24));
+            const monthsDifference = Math.ceil(timeDifference / (1000 * 60 * 60 * 24 * 30));
+            total += active.ratePerMonth * (daysDifference / (30 * monthsDifference));
+        }
+
     }
 
     return total.toFixed(2)
 }
 
-console.log(activatedDateParser(users, activatedUser))
+console.log(activatedDateParser(users, activatedUser)) // 7.91
+console.log(activatedDateParser(users2, activatedUser2)) // 14.65
